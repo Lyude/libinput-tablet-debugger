fb1c7a053e0c760b078a05471def69534f9033a1
Stephen Chandler Paul <thatslyude@gmail.com>
2014-05-31 23:25:21 -0400

    Change license from GPLv2 to X11/MIT

--- src/main.c
+++ src/main.c
@@ -1,22 +1,6 @@
 /*
- * Copyright ©2013 Lyude
- *
- * This file is free software: you may copy it, redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 2 of this License or (at your option) any
- * later version.
- *
- * This file is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * Some of this code has been adapted from event-debug.c from the libinput
- * project, to which the following copyrights and permissions apply:
- *
- * Copyright © 2013 Red Hat, Inc.
+ * Copyright © 2014 Lyude
+ * Copyright © 2014 Red Hat, Inc.
  *
  * Permission to use, copy, modify, distribute, and sell this software and
  * its documentation for any purpose is hereby granted without fee, provided
